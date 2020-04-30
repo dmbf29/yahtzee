de0372ae187248cacf32cbc43246b7301bd97cd6
Douglas Berkley <douglasmberkley@gmail.com>
2020-04-30 13:35:32 +0900

    added create and show to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,27 @@
 class GamesController < ApplicationController
+  before_action :get_game, only: [:show]
+
+  def create
+    @game = Game.new(game_params)
+    authorize @game
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render 'pages/home'
+    end
+  end
+
   def show
+    authorize @game
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:code, :name)
+  end
+
+  def get_game
+    @game = Game.find(params[:id])
   end
 end
