2599be291c11b74708a184733a6d7db678bde762
Douglas Berkley <douglasmberkley@gmail.com>
2020-04-30 23:13:13 +0900

    top half functionality working

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,20 @@
 class Category < ApplicationRecord
   has_many :submissions, dependent: :destroy
+
+  def get_total(game, user)
+    case name
+    when "Total"
+      user.submissions.joins(:category).where('categories.place < ? AND game_id = ?', 7, game.id).pluck(:value).sum
+    when "Total Top Half"
+      user.submissions.joins(:category).where('categories.place < ? AND game_id = ?', 8, game.id).pluck(:value).sum
+    when "Total Bot. Half"
+      # user.submissions.joins(:category).where('categories.place < ?', 7).pluck(:value).sum
+    when "Total Score"
+      # user.submissions.joins(:category).where('categories.place < ?', 7).pluck(:value).sum
+    end
+  end
+
+  def self.top_bonus
+    find_by_place(8)
+  end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,4 +4,21 @@ class Submission < ApplicationRecord
   belongs_to :user
   validates :value, numericality: true
   validates_uniqueness_of :category_id, scope: :user_id
+  after_save :check_top_bonus, if: :top_six?
+
+  def top_six?
+    category.place <= 6
+  end
+
+  def check_top_bonus
+    bonus_category = Category.top_bonus
+    top_total = user.submissions.joins(:category).where('categories.place < ? AND game_id = ?', 7, game.id).pluck(:value).sum
+    bonus_submission = Submission.where(
+      category: bonus_category,
+      user: user,
+      game: game
+    ).first_or_create
+    bonus_submission.value = top_total < 63 ? 0 : 35
+    bonus_submission.save
+  end
 end
