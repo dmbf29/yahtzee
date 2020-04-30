4edb99caa65c1839f69ee89f64d023101123e04a
Douglas Berkley <douglasmberkley@gmail.com>
2020-04-30 15:24:23 +0900

    Added test users

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2020_04_30_045808) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin", default: false
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
