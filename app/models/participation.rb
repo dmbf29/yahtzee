ff8f093ac2a5c4bb2d15206556f7d711ac658051
Douglas Berkley <douglasmberkley@gmail.com>
2020-04-30 15:32:24 +0900

    validation on paricipation

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,5 @@
 class Participation < ApplicationRecord
   belongs_to :game
   belongs_to :user
+  validates_uniqueness_of :game_id, scope: :user_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :games, only: [:create, :show]
+  resources :games, only: [:create, :show] do
+    resources :participations, only: [:create]
+  end
 end
