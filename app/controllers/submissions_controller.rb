d6f75e139b4e689dfd929580934c4ef59fa5432f
Douglas Berkley <douglasmberkley@gmail.com>
2020-04-30 18:26:19 +0900

    submissions create action

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,24 @@
 class SubmissionsController < ApplicationController
+  before_action :get_game, only: [:create]
+
+  def create
+    @submission = Submission.new(submission_params)
+    @submission.game = @game
+    authorize @submission
+    if @submission.save
+      redirect_to game_path(@game)
+    else
+      render 'games/show'
+    end
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:value, :category_id, :user_id)
+  end
+
+  def get_game
+    @game = Game.find(params[:game_id])
+  end
 end
