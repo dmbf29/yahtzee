53e5ec30af84c5a7c634147496a77c3f66441494
Douglas Berkley <douglasmberkley@gmail.com>
2020-05-02 12:37:24 +0900

    Added leaderboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :submissions
   has_many :games_won, source: :games, foreign_key: :winner_id
   validates :name, presence: true
+
+  def average_score
+    finished_games = participations.where.not(final_score: nil)
+    if finished_games.any?
+      (finished_games.pluck(:final_score).sum / finished_games.count).round(2)
+    else
+      'n/a'
+    end
+  end
 end
