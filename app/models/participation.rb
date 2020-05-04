3f3f30d1a1f453d0e8baea5c930789e993178db5
Douglas Berkley <douglasmberkley@gmail.com>
2020-05-04 20:41:00 +0900

    added turns

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,4 +2,8 @@ class Participation < ApplicationRecord
   belongs_to :game
   belongs_to :user
   validates_uniqueness_of :game_id, scope: :user_id
+
+  def submissions
+    game.submissions.where(user: user)
+  end
 end
