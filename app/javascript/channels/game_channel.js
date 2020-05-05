107bc24317e44593927f7dc4fddd6eea2f12a0b3
Douglas Berkley <douglasmberkley@gmail.com>
2020-05-05 12:29:35 +0900

    reversed order of submissions

--- app/javascript/channels/game_channel.js
+++ app/javascript/channels/game_channel.js
@@ -11,14 +11,14 @@ if (gameContainer) {
       const gameTable = document.getElementById('game-table');
       gameTable.innerHTML = data.table; // called when data is broadcast in the cable
       if (data.message) {
-        submissionsContainer.insertAdjacentHTML('beforeend', data.message);
+        submissionsContainer.insertAdjacentHTML('afterbegin', data.message);
         // submissionsContainer.style.backgroundColor = 'yellow'
       }
       initSortable();
       const submissions = document.querySelectorAll('.submission');
       const lastMessage = submissions[submissions.length - 1];
       if (lastMessage !== undefined) {
-        lastMessage.scrollIntoView();
+        // lastMessage.scrollIntoView();
       }
     },
   });
